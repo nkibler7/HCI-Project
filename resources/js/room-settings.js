cd176f077d952f08102ebf45b0a9ecf6007693cf
welshs16 <welshs16@vt.edu>
2015-04-12 17:28:50 -0400

    room specific settings pages done

--- resources/js/room-settings.js
+++ resources/js/room-settings.js
@@ -16,4 +16,38 @@ $(function() {
 			document.title = notifyParam.replace("_", " ") + "Settings";
 	}
 
+	$('.mode-select').click(function() {
+		var t = $(this);
+		t.siblings('.active').toggleClass('active');
+		t.toggleClass('active');
+
+		if (t.text() == "Manual") {
+			$('.adaptive').parent().hide();
+			$('.music-notif').attr('hidden', false);
+		}
+		if (t.text() == "Automatic") {
+			$('.adaptive').parent().show();
+			$('.music-notif').attr('hidden', true);
+		}
+	});
+
+	$('.save-btn').click(function() {
+		var t = $(this);
+		t.html('<i class="fa fa-spin fa-spinner"></i>');
+		t.attr('disabled', true);
+		setTimeout(function() {
+			t.removeAttr('disabled');
+			t.html('<i class="fa fa-check"></i> Save');
+		}, 500);
+	});
+
+	$('.revert-btn').click(function() {
+		var t = $(this);
+		t.html('<i class="fa fa-spin fa-spinner"></i>');
+		t.attr('disabled', true);
+		setTimeout(function() {
+			t.removeAttr('disabled');
+			t.html('<i class="fa fa-times"></i> Revert');
+		}, 500);
+	});
 });
